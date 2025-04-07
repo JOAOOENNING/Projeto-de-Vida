cb5552d5abccbb5324fea5748ca0173d924ac1fa
JOAOOENNING <146237359+JOAOOENNING@users.noreply.github.com>
2025-04-07 07:52:13 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const botoes = document.querySelectorAll("botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length; i++){
-    
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        botoes[i].classList.add("ativo");
-    }
-    console.log(i);
-}
+      for (let j = 0; j < botoes.length; j++) {
+        botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
+      }
+  
+      botoes[i].classList.add("ativo");
+      textos[i].classList.add("ativo");
+    };
+  }
